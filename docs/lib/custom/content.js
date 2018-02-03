44f9f5154e72dc20cd59c27aff81a642f120c46e
ssysm <syua9708@flhs.us>
2018-02-04 06:17:03 +0800

    update docs script

--- docs/lib/custom/content.js
+++ docs/lib/custom/content.js
@@ -11,4 +11,4 @@ var fs = require('fs');
  * into strings of those files' content before the file is delivered to a
  * browser: the content is read ahead-of-time and included in bundle.js.
  */
-module.exports = '# 简介\n' + fs.readFileSync('./content/introduction.md', 'utf8') + '\n' + '# 验证登陆\n' + fs.readFileSync('./content/auth.md', 'utf8') + '\n' + '# 活动\n' + fs.readFileSync('./content/activity.md', 'utf8') + '\n' + '# 募捐\n' + fs.readFileSync('./content/funding.md', 'utf8') + '\n' + '# 作品\n' + fs.readFileSync('./content/work.md', 'utf8') + '\n';
+module.exports = '# 简介\n' + fs.readFileSync('./content/introduction.md', 'utf8') + '\n' + '# 验证登陆\n' + fs.readFileSync('./content/auth.md', 'utf8') + '\n' + '# 活动\n' + fs.readFileSync('./content/activity.md', 'utf8') + '\n' + '# 募捐\n' + fs.readFileSync('./content/funding.md', 'utf8') + '\n' + '# 作品\n' + fs.readFileSync('./content/work.md', 'utf8') + '\n' + '# 版本\n' + fs.readFileSync('./content/version.md', 'utf8') + '\n' + '# 提问回答\n' + fs.readFileSync('./content/qa.md', 'utf8');
