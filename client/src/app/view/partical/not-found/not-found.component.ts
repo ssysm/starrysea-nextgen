a81e27af3101d16887619a5bba5f27b7d5a39457
ssysm <syua9708@flhs.us>
2018-02-10 13:29:58 -0500

    update not found cucuImg warning
    nsfw

--- client/src/app/view/partical/not-found/not-found.component.ts
+++ client/src/app/view/partical/not-found/not-found.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Http} from "@angular/http";
 import {environment} from "../../../../environments/environment";
 import {Meta, Title} from "@angular/platform-browser";
-
+declare var swal:any;
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
@@ -18,6 +18,7 @@ export class NotFoundComponent implements OnInit {
 
   cucuImg:string;
   apiBase:string = environment.apiBase;
+  nsfwButton:Boolean = true;
 
   ngOnInit() {
     this.titleService.setTitle('404 Not Found - Starry Sea Volunteers Association');
@@ -25,12 +26,27 @@ export class NotFoundComponent implements OnInit {
     this.metaService.updateTag({content: '404 Not Found'}, "property='og:title'");
     this.metaService.updateTag({content: 'Oops, Looks like this page doesn\'t exist'}, "property='og:description'");
     this.metaService.updateTag({content: 'Oops, Looks like this page doesn\'t exist'}, "name='description'");
-    this.
-      http
-        .get(environment.apiBase+'/errorImg',)
-      .subscribe(data=>{
-        this.cucuImg = data.text();
-      })
+
+  }
+
+  digCucuImg(){
+    swal({
+      title: "Are you sure?",
+      text: "The image might not suitable for viewing in public",
+      icon: "warning",
+      buttons: [true, "Yes!"]
+    })
+      .then((result) => {
+        if (result) {
+          this.nsfwButton = false;
+          this.
+          http
+            .get(environment.apiBase+'/errorImg')
+            .subscribe(data=>{
+              this.cucuImg = data.text();
+            })
+        }
+      });
   }
 
 }
