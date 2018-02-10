d7eb431224c8c3cad29a6b876f169a99274990d9
ssysm <syua9708@flhs.us>
2018-02-09 19:08:10 -0500

    update animation

--- client/src/app/common/animation/route.animation.ts
+++ client/src/app/common/animation/route.animation.ts
@@ -1,41 +1,51 @@
+import {trigger, animate, transition, style, query, group, keyframes} from '@angular/animations';
 
-import { trigger, animate, transition, style, query } from '@angular/animations';
-
-export const routeAnimation =
-
-  trigger('routeAnimation', [
-
-    transition( '* => *', [
-
-      query(':enter',
-        [
+export const routeAnimation = trigger('routeAnimation', [
+  transition('* <=> *', [
+    query(':enter, :leave', style({
+        position: 'fixed',
+        width: '100%'
+      })
+      , {
+        optional: true
+      }),
+    group([  // block executes in parallel
+      query(':enter', [
+        animate(300, keyframes([
           style({
             opacity: 0,
-            display: 'none'
-          })
-        ],
-        { optional: true }
-      ),
-
-      query(':leave',
-        [
+            display:'none'
+          }),
+          style({
+            opacity: 0.5,
+            display:'none'
+          }),
           style({
             opacity: 1,
-
+            display:'block'
+          })
+        ])),
+      ], {
+        optional: true
+      }),
+      query(':leave', [
+        animate(300, keyframes([
+          style({
+            opacity: 1,
+            display:'none'
           }),
-          animate('0.2s', style({ opacity: 0 }))
-        ],
-        { optional: true }
-      ),
-
-      query(':enter',
-        [
-          style({ opacity: 0 }),
-          animate('0.2s', style({ opacity: 1 }))
-        ],
-        { optional: true }
-      )
-
+          style({
+            opacity: 0.5,
+            display:'none'
+          }),
+          style({
+            opacity: 0,
+            display:'none'
+          })
+        ]))
+      ], {
+        optional: true
+      }),
     ])
-
-  ]);
+  ])
+]);
