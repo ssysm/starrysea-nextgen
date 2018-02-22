5f50907adc6e9197a88b6446a3ba08f706109f79
null <server@theeditorstudio.com>
2018-02-22 04:02:37 +0000

    server fix

--- client/proxy.js
+++ client/proxy.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const axios = require('axios');
 
+const appUri = "https://starrysea.org/"
 const renderUri = "http://localhost:8080/render";
 
 const app = express();
 
 app.get('*',(req,res)=>{
 
-  axios.get(`${renderUri}/${req.protocol + '://' + req.get('host') + req.originalUrl}`)
-    .then(data=>{
+  axios.get(`${renderUri}/${appUri + req.originalUrl}`)
+    .then((data)=>{
       res.set('Cache-Control','public, max-age=300, s-maxage=600');
-      res.send(data.toString());
+      res.send(data.data.toString());
     })
 
 });
