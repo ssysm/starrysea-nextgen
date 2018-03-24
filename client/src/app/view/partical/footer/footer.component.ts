4d0d9cf20ad152955944332cbde69218d2cc3736
theeditor <syua9708@flhs.us>
2018-03-24 12:24:05 -0400

    update api and locale change

--- client/src/app/view/partical/footer/footer.component.ts
+++ client/src/app/view/partical/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CookieService} from "../../../service/cookie.service";
+import {LocaleService} from "../../../service/locale.service";
 declare var $:any;
 @Component({
   selector: 'app-footer',
@@ -9,15 +10,18 @@ declare var $:any;
 export class FooterComponent implements OnInit {
 
   constructor(
-    private cookie:CookieService
+    private cookie:CookieService,
+    private locale:LocaleService
   ) { }
 
   ngOnInit() {
   }
 
   selectLang(locale:string):void{
+    this.cookie.deleteCookie();
     this.cookie.setCookie("lang",locale,9999);
-    window.location.reload();
+    this.cookie.setCookie("auto_jump","false",9999);
+    this.locale.onInit();
   }
 
 }
