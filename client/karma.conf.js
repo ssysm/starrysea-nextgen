b037b960c75233b36cc283850bb5bee83cae365c
ssysm <me@theeditorstudio.com>
2018-05-23 21:06:39 -0400

    update ci

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,7 +12,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-webpack/plugins/karma')
+      require('@angular-devkit/build-webpack/plugins/karma'),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -37,7 +39,7 @@ module.exports = function (config) {
           '--no-sandbox',
           '--remote-debugging-port=9222',
         ]
-      }
+      },
     },
     browsers: ['Chrome','ChromeHeadless'],
   });
