bba67060dd6c8510edc6a3bc4f6f8b106be77c9a
ssysm <me@theeditorstudio.com>
2018-06-03 00:52:02 -0400

    add remove file function

--- server/actions/activity/delete.js
+++ server/actions/activity/delete.js
@@ -2,40 +2,48 @@
 const Activity = require('../../models/Activity');
 const Funding = require('../../models/Funding');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 //Delete Activity
 //@method:DELETE
 //@body:uid
 //@return:JSON Object
 deleteActivity = (req,res)=>{
     "use strict";
-    Activity.remove({
+    Activity.findOne({
         _id:mongoose.Types.ObjectId(req.body.uid)
-    },(err,docs)=>{
-        if(err){
-            res.status(500).json({
-                success:false,
-                response:err
-            })
-        }else{
-            Funding.remove({
-                activity_id:req.body.uid
-            },(err2,fund)=>{
-                if(err2){
-                    res.status(500).json({
-                        success:false,
-                        response:err2
-                    })
-                }else{
-                    res.status(200).json({
-                        success:true,
-                        response:{
-                            q1:docs,
-                            q2:fund
-                        }
-                    })
-                }
-            })
-        }
-    })
+    },(err3,docs3)=>{
+        if(err3){console.log(err3)}
+        fs.unlinkSync(path.join(__dirname+'/../../public/activity/'+docs3.cover));
+        Activity.remove({
+            _id:mongoose.Types.ObjectId(req.body.uid)
+        },(err,docs)=>{
+            if(err){
+                res.status(500).json({
+                    success:false,
+                    response:err
+                })
+            }else{
+                Funding.remove({
+                    activity_id:req.body.uid
+                },(err2,fund)=>{
+                    if(err2){
+                        res.status(500).json({
+                            success:false,
+                            response:err2
+                        })
+                    }else{
+                        res.status(200).json({
+                            success:true,
+                            response:{
+                                q1:docs,
+                                q2:fund
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    });
 };
 module.exports = deleteActivity;
--- server/actions/work/delete.js
+++ server/actions/work/delete.js
@@ -1,26 +1,39 @@
 const works = require('../../models/Works');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 //Delete Work
 //@method:DELETE
 //@body:uid
 //@return:JSON Object
 deleteWork = (req,res)=>{
     "use strict";
-    works.remove({
+    works.findOne({
         _id:mongoose.Types.ObjectId(req.body.uid)
-    },(err,docs)=>{
-        if(err){
-            res.status(500).json({
-                success:false,
-                response:err
-            })
-        }else{
-            res.status(200).json({
-                success:true,
-                response:docs
-            })
+    },(err2,docs2)=>{
+        if(err2){console.log(err2)}
+        fs.unlinkSync(path.join(__dirname+'/../../public/work/'+docs2.file.cover));
+        fs.unlinkSync(path.join(__dirname+'/../../public/work/'+docs2.file.pdf));
+        for(let obj of docs2.file.images){
+            fs.unlinkSync(path.join(__dirname+'/../../public/work/'+obj.filename));
         }
-    })
+        works.remove({
+            _id:mongoose.Types.ObjectId(req.body.uid)
+        },(err,docs)=>{
+            if(err){
+                res.status(500).json({
+                    success:false,
+                    response:err
+                })
+            }else{
+                res.status(200).json({
+                    success:true,
+                    response:docs
+                })
+            }
+        })
+    });
+
 };
 
 module.exports = deleteWork;
